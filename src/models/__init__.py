81c22e555d5045b46fd5e92b4d595ca841969953
Vladimir Chernov <vecher.1@gmail.com>
2023-10-17 03:40:24 +0300

    update

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,11 @@
 __all__ = [
     "Base",
     "URLs",
-    "URLLogger"
+    "URLLogger",
+    "USERs"
 ]
 
 from .base import Base
 from .urls import URLs
 from .urllogger import URLLogger
+from .users import USERs
---
+++ src/models/users.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from datetime import datetime
+
+from .base import Base
+
+
+class USERs(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_name = Column(String(256), unique=True, nullable=False)
+    secret_hashed = Column(String(128))
+    created_at = Column(DateTime, index=True, default=datetime.utcnow)
